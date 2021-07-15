487793890e94090cbc0284c2bb8b8ac786869e57
khanh510 <buiduykhanh994994@gmail.com>
2021-07-15 15:55:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
-	
-</body>
-</html>
 <?php 
 require __DIR__ . '/core.php';
 require __DIR__ . '/vendor/autoload.php'; 
@@ -23,20 +20,28 @@ if( !file_exists($tokenPath) ){
 	$client = display_data();
 	$service = new Google_Service_Calendar($client);
 
+	$date = date("Y-m-d",strtotime("this week"));
+ 	$start = date("c", strtotime("-1 day",strtotime($date)) );
+ 	$end = date("c", strtotime("+6 days",strtotime($date)) );
+
 	// Print the next 10 events on the user's calendar.
 	$calendarId = 'primary';
 	$optParams = array(
 	  'maxResults' => 10,
 	  'orderBy' => 'startTime',
 	  'singleEvents' => true,
-	  'timeMin' => date('c'),
+	  'timeMin' => $start,
+	  'timeMax'	=> $end
 	);
 	$results = $service->events->listEvents($calendarId, $optParams);
 	$events = $results->getItems();
 	echo "<pre>";
 	echo json_encode($events);
+	// print_r($service);
 	echo "</pre>";
 
 }
 
  ?>
+</body>
+</html>
