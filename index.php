0f46eac8325b09bb261e8ad7c9ae09adef62132f
khanh510 <40050500+khanh510@users.noreply.github.com>
2021-07-15 17:04:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,16 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
+	<form action="" method="get">
+		<div class="form-group">
+			<label for="date"></label>
+			<input type="date" name="date" id="date" class="form-control">	
+		</div>
+		<div class="form-group">
+			<button type="submit" class="btn btn-primary">Search</button>
+		</div>
+		
+	</form>
 <?php 
 require __DIR__ . '/core.php';
 require __DIR__ . '/vendor/autoload.php'; 
@@ -19,10 +29,17 @@ if( !file_exists($tokenPath) ){
 }else{
 	$client = display_data();
 	$service = new Google_Service_Calendar($client);
-
-	$date = date("Y-m-d",strtotime("this week"));
- 	$start = date("c", strtotime("-1 day",strtotime($date)) );
- 	$end = date("c", strtotime("+6 days",strtotime($date)) );
+	if( isset($_GET['date'])&& !empty($_GET['date']) ){
+		$date = $_GET['date'];
+		$date = date("Y-m-d",strtotime("this week",strtotime($date)));
+	 	$start = date("c", strtotime("-1 day",strtotime($date)) );
+	 	$end = date("c", strtotime("+5 days",strtotime($date)) );
+	}else{
+		$date = date("Y-m-d",strtotime("this week"));
+	 	$start = date("c", strtotime("-1 day",strtotime($date)) );
+	 	$end = date("c", strtotime("+6 days",strtotime($date)) );
+	}
+	
 
 	// Print the next 10 events on the user's calendar.
 	$calendarId = 'primary';
@@ -36,7 +53,7 @@ if( !file_exists($tokenPath) ){
 	$results = $service->events->listEvents($calendarId, $optParams);
 	$events = $results->getItems();
 	echo "<pre>";
-	echo json_encode($events);
+	print_r($events);
 	// print_r($service);
 	echo "</pre>";
 
